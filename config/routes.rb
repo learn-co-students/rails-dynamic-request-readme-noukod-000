f967cb5a3025e8039f4ccd7f97ca937a5f005a40
Jean Gérard Bousiquot <jgb@noukod.com>
2019-01-27 17:02:05 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,6 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-  get '/posts/:id', to: 'posts#show'
+  # get '/posts/:id', to: 'posts#show'
+  resources :posts, only: :show
 end
