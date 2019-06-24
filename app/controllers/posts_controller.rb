a87c61477cc9804b44dfc935612abc5948bbad8f
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-06-24 23:25:30 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,7 @@
-class CreatePosts < ActiveRecord::Migration
-  def change
-    create_table :posts do |t|
-      t.string :title
-      t.text :description
+class PostsController < ApplicationController
 
-      t.timestamps null: false
+    def show
+        @post = Post.find(params[:id])
     end
-  end
+    
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,3 @@
 class Post < ActiveRecord::Base
 
-  def show
-    @post = Post.find(params[:id])
-  end
-
 end
